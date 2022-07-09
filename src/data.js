697b0f7103dcbb9f207adfa3637fe6fcc2d83ddb
dqarias <dino.quispea@gmail.com>
2022-07-09 16:04:47 -0500

    Add react router for navigation

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    title: 'The power of self discipline',
+    author: 'Peter Hollins',
+    genre: 'Self-Help',
+  },
+  {
+    id: 2,
+    title: 'Get out your own way',
+    author: 'Mark Goulston',
+    genre: 'Self-Help',
+  },
+  {
+    id: 3,
+    title: 'Life of Pi',
+    author: 'Yann Martel',
+    genre: 'Adventure',
+  },
+];
