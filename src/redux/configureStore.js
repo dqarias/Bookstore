91c68f6c08684706f0f37c87a1d57041f317c041
dqarias <dino.quispea@gmail.com>
2022-07-12 11:27:54 -0500

    Update configureStore and remove combinedReducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
 import { booksReducer } from './books/books';
 import { categoriesReducer } from './categories/categories';
 
-
 const store = configureStore({
-    reducer : {
-        books: booksReducer,
-        categories: categoriesReducer,
-    }
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
 });
 
 export default store;
