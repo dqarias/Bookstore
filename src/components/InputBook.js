2a3477e6914da1b62f7588b29ee4b7bd041d96de
dqarias <dino.quispea@gmail.com>
2022-07-12 17:02:06 -0500

    Add useSelector on Books component

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
-const InputBook = () => (
-  <form>
-    <input type="text" placeholder="Book title" />
-    <input type="text" placeholder="Book author" />
-    <button type="button">ADD BOOK</button>
-  </form>
-);
+const InputBook = () => {
+  const onChange = () => {
+
+  };
+
+  return (
+    <form>
+      <input type="text" placeholder="Book title" />
+      <input type="text" placeholder="Book author" />
+      <button type="button">ADD BOOK</button>
+    </form>
+  );
+};
 
 export default InputBook;
